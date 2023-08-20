e40fad02fc92852783214dc8ea7eaa923e94bf25
mlvillarroya <mlvillarroya@gmail.com>
2023-08-20 11:20:25 +0200

    one answer question restyled

--- MoodleQuestions/Question_answer/Question_types/OneAnswer.py
+++ MoodleQuestions/Question_answer/Question_types/OneAnswer.py
@@ -1,14 +1,15 @@
-from MoodleQuestions.Base.Question import Question
+"""One answer questions. Inherits from Question"""
+from MoodleQuestions.Question_answer import Question
 
 class OneAnswer(Question):
-
+    """Class OneAnswer inherits from Question"""
     def __init__(self,category,code,question,answersList,generalFeedback):
-        Question.__init__(self,category,code,question,answersList,generalFeedback)
-        
-    def create_answer_from_list(self):
-        completeAnswer = ''
-        for index,answer in enumerate(self.__answers_list):
-            nextLine = "\n" if index < len(self.__answers_list) - 1 else ''
-            completeAnswer = completeAnswer + "=%100%" + answer.answer + nextLine
-        return completeAnswer
+        super().__init__(category,code,question,answersList,generalFeedback)
 
+    def create_answer_from_list(self):
+        """Function to create custom text for one answer questions"""
+        complete_answer = ''
+        for index,answer in enumerate(self.__answers_list):
+            next_line = "\n" if index < len(self.__answers_list) - 1 else ''
+            complete_answer = complete_answer + "=%100%" + answer.answer + next_line
+        return complete_answer
