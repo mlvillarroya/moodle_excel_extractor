b63eb021c7cb195fcf4a064f083c6a89a169ead1
mlvillarroya <mlvillarroya@gmail.com>
2023-08-22 13:55:34 +0200

    numeric array refactored

--- MoodleObjects/Arrays/NumericArray.py
+++ MoodleObjects/Arrays/NumericArray.py
@@ -1,21 +1,25 @@
-from MoodleQuestions.Base.Answer import Answer
-from MoodleQuestions.Base.Numeric import Numeric
+from MoodleObjects.Answers import Answer
+from MoodleObjects.Questions import Numeric
 import misc.Constants as CS
-from MoodleQuestions.Array.QuestionsArray import QuestionsArray
+from .Base.BaseArray import BaseArray
 
+class NumericArray(BaseArray):
+    """Class for numeric questions array"""
 
-class NumericArray(QuestionsArray):
-    def __init__(self, questionsArray=[], successfullAnswers=0, failedAnswers=0):
-        super().__init__(questionsArray, successfullAnswers, failedAnswers)
- 
-    def ExtractCategory(self,question, mainCategory):
-        return mainCategory + '/' + question[CS.NUMERIC_SUBCATEGORY_TITLE] if question[CS.TRUE_FALSE_SUBCATEGORY_TITLE] else mainCategory
-    def ExtractAnswerList(self, question):
-        if question[CS.NUMERIC_TOLERANCE_TITLE] is None: question[CS.NUMERIC_TOLERANCE_TITLE] = 0
-        return [Answer(question[CS.NUMERIC_CORRECT_VALUE_TITLE],numericTolerance=question[CS.NUMERIC_TOLERANCE_TITLE])]
-    def CreateQuestion(self,category,question,answer):
+    def extract_category(self,question, main_category):
+        return main_category + '/' + \
+            question[CS.NUMERIC_SUBCATEGORY_TITLE] if question[CS.TRUE_FALSE_SUBCATEGORY_TITLE] \
+            else main_category
+
+    def extract_answer_list(self, question):
+        if question[CS.NUMERIC_TOLERANCE_TITLE] is None: 
+            question[CS.NUMERIC_TOLERANCE_TITLE] = 0
+        return [Answer(question[CS.NUMERIC_CORRECT_VALUE_TITLE],
+                       numeric_tolerance = question[CS.NUMERIC_TOLERANCE_TITLE])]
+
+    def create_question(self,category,question,answers):
         return Numeric(category,\
-                        'N'+str(self.successfullAnswers),\
+                        'N' + str(self.successfull_answers),\
                         question[CS.NUMERIC_QUESTION_TITLE],\
-                        answer,\
+                        answers,\
                         question[CS.NUMERIC_FEEDBACK_TITLE])
