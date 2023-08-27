3dc023a533db0d50c583e16bce69ea19def304b0
Mikel López <mlvillarroya@gmail.com>
2023-08-27 22:16:24 +0200

    excel styling refactoring

--- excel/ExcelCreator/ExcelCellStyling.py
+++ excel/ExcelCreator/ExcelCellStyling.py
@@ -6,24 +6,30 @@ from openpyxl.worksheet.worksheet import Worksheet
 
 class ExcelCellStyling:
     """Class for excel styling"""
-    def __change_cell_background_and_text_colors(self,cell,backgroundColor='FFFFFF',textColor='000000'):
-        cell.fill = PatternFill("solid", start_color="E7AA73")
-        cell.font=Font(b=True, color="653159")
-    
+    def __change_cell_background_and_text_colors(self, cell, background_color='FFFFFF', text_color='000000'):
+        cell.fill = PatternFill("solid", start_color= background_color)
+        cell.font = Font(b= True, color= text_color)
+
     def __apply_full_border_to_cell(self, cell):
-        cell.border = Border(left=Side(style='thin'), 
-                     right=Side(style='thin'), 
-                     top=Side(style='thin'), 
+        cell.border = Border(left=Side(style='thin'),
+                     right=Side(style='thin'),
+                     top=Side(style='thin'),
                      bottom=Side(style='thin'))
 
-    def __change_cell_alignment(self, cell, horizontal_alignment='left',vertical_alignment='top',wrap_text = True):
-        cell.alignment = Alignment(horizontal= horizontal_alignment, vertical= vertical_alignment, wrap_text= wrap_text)
+    def __change_cell_alignment(self,
+                                cell,
+                                horizontal_alignment='left',
+                                vertical_alignment='top',
+                                wrap_text = True):
+        cell.alignment = Alignment(horizontal= horizontal_alignment,
+                                   vertical= vertical_alignment,
+                                   wrap_text= wrap_text)
 
-    def __adjust_sheet_row_size(self, ws, row, height):
-        ws.row_dimensions[row].height = height
+    def __adjust_sheet_row_size(self, sheet, row, height):
+        sheet.row_dimensions[row].height = height
 
-    def __adjust_sheet_column_size(self, ws,column,width):
-        ws.column_dimensions[column].width = width
+    def __adjust_sheet_column_size(self, sheet, column, width):
+        sheet.column_dimensions[column].width = width
 
     def first_row_adequation(self, sheet: Worksheet, last_column_letter):
         """Editing first row with border and background"""
