4395aa79c53eb18c7710cbf24b2b99ac63f1576e
Mikel López <mlvillarroya@gmail.com>
2023-09-03 10:21:22 +0200

    adding titles to base array

--- MoodleObjects/Arrays/Base/BaseArray.py
+++ MoodleObjects/Arrays/Base/BaseArray.py
@@ -2,11 +2,12 @@
 import misc.Constants as CS
 class BaseArray:
     """Class for base arrays"""
-    def __init__(self, dictionary_array, main_category = 'Category'):
+    def __init__(self, dictionary_array, main_category = 'Category', array_name = 'base_array', constants={}):
         if  not self.__is_list_of_dict(dictionary_array):
             raise ValueError(CS.EXCEPTION_LIST_DICT_NEEDED)
+        self.__titles = self._extract_titles_from_constants(array_name, constants)
         self.__questions_array = []
-        self.__successfull_answers = 0
+        self.__successful_answers = 0
         self.__failed_answers = 0
         for question in dictionary_array:
             try:
@@ -14,7 +15,7 @@ class BaseArray:
             except Exception:
                 self.__failed_answers += 1
             else:
-                self.__successfull_answers += 1
+                self.__successful_answers += 1
 
     @property
     def question_array(self):
@@ -23,13 +24,17 @@ class BaseArray:
 
     @property
     def successful_answers(self):
-        """Property: successfull answers"""
-        return self.__successfull_answers
+        """Property: successful answers"""
+        return self.__successful_answers
 
     @property
     def failed_answers(self):
         """Property: questions array"""
         return self.__failed_answers
+    
+    @property
+    def titles(self):
+        return self.__titles
 
     def __create_question(self, question, main_category= 'Category'):
         """Function to create the questions"""
@@ -38,15 +43,15 @@ class BaseArray:
         return self._extract_question(category,question,answer)
 
     def _extract_category(self, question, main_category):
-        """MUST BE OVERRIDEN Function: extract main category"""
+        """MUST BE OVERRIDDEN Function: extract main category"""
         return ''
 
     def _extract_answer_list(self, question):
-        """MUST BE OVERRIDEN Function: extract answer list"""
+        """MUST BE OVERRIDDEN Function: extract answer list"""
         return ''
 
     def _extract_question(self,category, question, answers):
-        """MUST BE OVERRIDEN Function: create question"""
+        """MUST BE OVERRIDDEN Function: create question"""
         return ''
 
     def __is_list_of_dict(self,dict_array):
@@ -64,3 +69,6 @@ class BaseArray:
         for question in self.__questions_array:
             questions_printed += question.print_question() + '\n'
         return  questions_printed
+
+    def _extract_titles_from_constants(self,array_name, constants):
+        return {key:constants[array_name]["cell_titles"][key][1] for key in constants[array_name]["cell_titles"].keys()}
--- MoodleObjects/Arrays/MultipleChoiceArray.py
+++ MoodleObjects/Arrays/MultipleChoiceArray.py
@@ -6,33 +6,35 @@ from .Base.BaseArray import BaseArray
 
 with open("static/excel_creation_constants.json", "r", encoding="utf8") as file:
     constants = json.load(file)
-    mc_titles = {key:constants["multiple_choice_sheet"]["cell_titles"][key][1] for key in constants["multiple_choice_sheet"]["cell_titles"].keys()}
 
 class MultipleChoiceArray(BaseArray):
     """Class for multiple choice questions array"""
+    
+    def __init__(self, dictionary_array, main_category = 'Category'):
+        super().__init__(dictionary_array, main_category,'multiple_choice_sheet', constants)
 
     def _extract_category(self, question, main_category):
         """Function to extract category"""
-        subcategory = question[mc_titles["subcategory_title"]] or None
+        subcategory = question[self.titles["subcategory_title"]] or None
         return main_category + '/' + subcategory if subcategory else main_category
 
     def _extract_answer_list(self, question: dict):
         """Function to extract answer list"""
         answers = []
-        if not mc_titles["bad_answer_points_title"] in question.keys():
-            question[mc_titles["bad_answer_points_title"]] = '0'
-        for i, answer in enumerate([question[mc_titles["correct_answer_title"]],\
-                                    question[mc_titles["incorrect_answer_1_title"]],\
-                                    question[mc_titles["incorrect_answer_2_title"]],\
-                                    question[mc_titles["incorrect_answer_3_title"]],\
-                                    question[mc_titles["incorrect_answer_4_title"]]
+        if not self.titles["bad_answer_points_title"] in question.keys():
+            question[self.titles["bad_answer_points_title"]] = '0'
+        for i, answer in enumerate([question[self.titles["correct_answer_title"]],\
+                                    question[self.titles["incorrect_answer_1_title"]],\
+                                    question[self.titles["incorrect_answer_2_title"]],\
+                                    question[self.titles["incorrect_answer_3_title"]],\
+                                    question[self.titles["incorrect_answer_4_title"]]
                                     ]):
             if i in (0,1) and answer is None:
                 raise ValueError(CS.EXCEPTION_NONE_VALUE)
             elif i == 0:
                 created_answer = Answer(answer,value="100")
             elif answer is not None:
-                created_answer = Answer(answer,value=question[mc_titles["bad_answer_points_title"]])
+                created_answer = Answer(answer,value=question[self.titles["bad_answer_points_title"]])
             else: continue
             answers.append(created_answer)
         return answers
@@ -40,6 +42,6 @@ class MultipleChoiceArray(BaseArray):
     def _extract_question(self, category, question, answers) -> MultipleChoice:
         return MultipleChoice(category,\
                             'MC'+ str(self.successful_answers),\
-                            question[mc_titles["question_name_title"]],\
+                            question[self.titles["question_name_title"]],\
                             answers,\
-                            question[mc_titles["feedback_title"]])
+                            question[self.titles["feedback_title"]])
