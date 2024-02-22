31693b9060d39be0c1af89fa09a7bd8b60ee75d6
Mikel López <mlvillarroya@gmail.com>
2024-02-22 11:09:30 +0100

    first shit version

--- main.py
+++ main.py
@@ -1,35 +1,105 @@
 from excel import ExcelCreator, ExcelExtractor
 from MoodleObjects import MultipleChoiceArray, NumericArray, OneAnswerArray, TrueFalseArray
 from misc import FileUtils
+
 # create a demo excel file
-excel_file = ExcelCreator(demo_data=True,
-                          create_multiple_choice=True,
-                          create_numeric=True,
-                          create_one_answer=True,
-                          create_true_false=True)
-excel_file.save_excel_file()
+# excel_file = ExcelCreator(demo_data=True,
+#                           create_multiple_choice=True,
+#                           create_numeric=True,
+#                           create_one_answer=True,
+#                           create_true_false=True)
+# excel_file.save_excel_file()
+#
+# # extract questions from the demo excel file
+# excel_extractor = ExcelExtractor(excel_file.full_path)
+# excel_questions = excel_extractor.extract_questions_from_workbook()
+#
+# # excel_questions is a dictionary. Each one of the keys to an object
+# array_of_answers = []
+# if "Multiple choice" in excel_questions:
+#     multiple_choice_array = MultipleChoiceArray(excel_questions["Multiple choice"])
+#     array_of_answers.append(multiple_choice_array)
+# if "Numeric" in excel_questions:
+#     numeric_array = NumericArray(excel_questions["Numeric"])
+#     array_of_answers.append(numeric_array)
+# if "One answer" in excel_questions:
+#     one_answer_array = OneAnswerArray(excel_questions["One answer"])
+#     array_of_answers.append(one_answer_array)
+# if "True_false" in excel_questions:
+#     true_false_array = TrueFalseArray(excel_questions["True_false"])
+#     array_of_answers.append(true_false_array)
+#
+# answer = []
+# for element in array_of_answers:
+#     answer.append(element.print_all_questions())
+#
+# FileUtils.create_txt_file("answers.txt", "\n".join(answer))
 
-# extract questions from the demo excel file
-excel_extractor = ExcelExtractor(excel_file.full_path)
-excel_questions = excel_extractor.extract_questions_from_workbook()
+# GUI creation
 
-# excel_questions is a dictionary. Each one of the keys to an object
-array_of_answers = []
-if "Multiple choice" in excel_questions:
-    multiple_choice_array = MultipleChoiceArray(excel_questions["Multiple choice"])
-    array_of_answers.append(multiple_choice_array)
-if "Numeric" in excel_questions:
-    numeric_array = NumericArray(excel_questions["Numeric"])
-    array_of_answers.append(numeric_array)
-if "One answer" in excel_questions:
-    one_answer_array = OneAnswerArray(excel_questions["One answer"])
-    array_of_answers.append(one_answer_array)
-if "True_false" in excel_questions:
-    true_false_array = TrueFalseArray(excel_questions["True_false"])
-    array_of_answers.append(true_false_array)
+import tkinter as tk
+import tkinter.ttk as ttk
 
-answer = []
-for element in array_of_answers:
-    answer.append(element.print_all_questions())
+# Creación de la ventana principal
+root = tk.Tk()
+root.title("Excel creator")
+root.geometry("600x400")
 
-FileUtils.create_txt_file("answers.txt", "\n".join(answer))
+# Creación del frame principal
+main_frame = tk.Frame(root, padx=10, pady=0)
+main_frame.pack(fill=tk.BOTH, expand=True)
+
+notebook = ttk.Notebook(main_frame)
+# Creación de las pestañas
+create_excel_tab = tk.Frame(notebook)
+extract_excel_tab = tk.Frame(notebook)
+help_tab = tk.Frame(notebook)
+
+# Añadir las pestañas al notebook
+
+notebook.add(create_excel_tab, text="Create excel")
+notebook.add(extract_excel_tab, text="Extract excel")
+notebook.add(help_tab, text="Help")
+notebook.pack(fill=tk.BOTH, expand=True)
+
+# Diseño de la primera pestaña
+# Frame izquierdo
+first_tab_left_frame = tk.Frame(create_excel_tab)
+first_tab_left_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+first_tab_left_frame_title = tk.Label(first_tab_left_frame, text="Título")
+first_tab_left_frame_title.pack(fill=tk.BOTH, expand=True)
+
+first_tab_left_frame_option_1 = tk.Checkbutton(first_tab_left_frame, text="Opción 1")
+first_tab_left_frame_option_1.pack(fill=tk.BOTH, expand=True)
+
+first_tab_left_frame_option_2 = tk.Checkbutton(first_tab_left_frame, text="Opción 2")
+first_tab_left_frame_option_2.pack(fill=tk.BOTH, expand=True)
+
+first_tab_left_frame_option_3 = tk.Checkbutton(first_tab_left_frame, text="Opción 3")
+first_tab_left_frame_option_3.pack(fill=tk.BOTH, expand=True)
+
+first_tab_left_frame_option_4 = tk.Checkbutton(first_tab_left_frame, text="Opción 4")
+first_tab_left_frame_option_4.pack(fill=tk.BOTH, expand=True)
+
+# Frame derecho
+first_tab_right_frame = tk.Frame(create_excel_tab)
+first_tab_right_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+
+first_tab_right_frame_title_1 = tk.Label(first_tab_right_frame, text="Título")
+first_tab_right_frame_title_1.pack(fill=tk.BOTH, expand=True)
+
+first_tab_right_frame_option_1 = tk.Checkbutton(first_tab_right_frame, text="Opción 1")
+first_tab_right_frame_option_1.pack(fill=tk.BOTH, expand=True)
+
+first_tab_right_frame_title_2 = tk.Label(first_tab_right_frame, text="Título")
+first_tab_right_frame_title_2.pack(fill=tk.BOTH, expand=True)
+
+first_tab_right_frame_text_block = tk.Entry(first_tab_right_frame)
+first_tab_right_frame_text_block.pack(fill=tk.BOTH, expand=True)
+
+first_tab_right_frame_button = tk.Button(first_tab_right_frame, text="Botón")
+first_tab_right_frame_button.pack(fill=tk.BOTH, expand=True)
+
+# Ejecución de la ventana principal
+root.mainloop()
