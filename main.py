174cd3ad2c532177a3697456f594628b6f130f4a
Mikel López <mlvillarroya@gmail.com>
2024-02-23 13:50:36 +0100

    first tab almost ready. Only generate button required

--- main.py
+++ main.py
@@ -43,11 +43,12 @@ import tkinter.ttk as ttk
 # Creación de la ventana principal
 root = tk.Tk()
 root.title("Excel creator")
-root.geometry("600x400")
+root.geometry("500x204")
 
 # Creación del frame principal
 main_frame = tk.Frame(root, padx=10, pady=0)
 main_frame.pack(fill=tk.BOTH, expand=True)
+# Asignar el mismo peso a cada columna del frame principal
 
 notebook = ttk.Notebook(main_frame)
 # Creación de las pestañas
@@ -57,49 +58,70 @@ help_tab = tk.Frame(notebook)
 
 # Añadir las pestañas al notebook
 
-notebook.add(create_excel_tab, text="Create excel")
+notebook.add(create_excel_tab, text="Create excel", padding=20)
 notebook.add(extract_excel_tab, text="Extract excel")
 notebook.add(help_tab, text="Help")
 notebook.pack(fill=tk.BOTH, expand=True)
 
 # Diseño de la primera pestaña
+# Hay un frame con el título y otro frame con dos subframes
+
 # Frame izquierdo
 first_tab_left_frame = tk.Frame(create_excel_tab)
-first_tab_left_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-
-first_tab_left_frame_title = tk.Label(first_tab_left_frame, text="Título")
-first_tab_left_frame_title.pack(fill=tk.BOTH, expand=True)
-
-first_tab_left_frame_option_1 = tk.Checkbutton(first_tab_left_frame, text="Opción 1")
-first_tab_left_frame_option_1.pack(fill=tk.BOTH, expand=True)
-
-first_tab_left_frame_option_2 = tk.Checkbutton(first_tab_left_frame, text="Opción 2")
-first_tab_left_frame_option_2.pack(fill=tk.BOTH, expand=True)
-
-first_tab_left_frame_option_3 = tk.Checkbutton(first_tab_left_frame, text="Opción 3")
-first_tab_left_frame_option_3.pack(fill=tk.BOTH, expand=True)
-
-first_tab_left_frame_option_4 = tk.Checkbutton(first_tab_left_frame, text="Opción 4")
-first_tab_left_frame_option_4.pack(fill=tk.BOTH, expand=True)
-
+first_tab_left_frame.pack(side="left", fill=tk.BOTH, expand=True)
 # Frame derecho
-first_tab_right_frame = tk.Frame(create_excel_tab)
-first_tab_right_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+first_tab_center_frame = tk.Frame(create_excel_tab)
+first_tab_center_frame.pack(side="right", fill=tk.BOTH, expand=True)
 
-first_tab_right_frame_title_1 = tk.Label(first_tab_right_frame, text="Título")
-first_tab_right_frame_title_1.pack(fill=tk.BOTH, expand=True)
+# El frame izquierdo tiene tres frames dentro
+first_tab_left_frame_subframe_1 = tk.Frame(first_tab_left_frame)
+first_tab_left_frame_subframe_2 = tk.Frame(first_tab_left_frame)
 
-first_tab_right_frame_option_1 = tk.Checkbutton(first_tab_right_frame, text="Opción 1")
-first_tab_right_frame_option_1.pack(fill=tk.BOTH, expand=True)
+first_tab_left_frame_subframe_1.pack(fill=tk.BOTH, expand=True)
+first_tab_left_frame_subframe_2.pack(fill=tk.BOTH, expand=True)
 
-first_tab_right_frame_title_2 = tk.Label(first_tab_right_frame, text="Título")
-first_tab_right_frame_title_2.pack(fill=tk.BOTH, expand=True)
+# El frame derecho tiene tres frames dentro
+first_tab_center_frame_subframe_1 = tk.Frame(first_tab_center_frame)
+first_tab_center_frame_subframe_2 = tk.Frame(first_tab_center_frame)
 
-first_tab_right_frame_text_block = tk.Entry(first_tab_right_frame)
-first_tab_right_frame_text_block.pack(fill=tk.BOTH, expand=True)
+first_tab_center_frame_subframe_1.pack(fill=tk.BOTH, expand=True)
+first_tab_center_frame_subframe_2.pack(fill=tk.BOTH, expand=True)
 
-first_tab_right_frame_button = tk.Button(first_tab_right_frame, text="Botón")
-first_tab_right_frame_button.pack(fill=tk.BOTH, expand=True)
+is_multiple_choice = tk.BooleanVar()
+is_true_false = tk.BooleanVar()
+is_numeric = tk.BooleanVar()
+is_one_answer = tk.BooleanVar()
+is_demo_data = tk.BooleanVar()
+
+first_tab_left_frame_title = tk.Label(first_tab_left_frame_subframe_1, text="Question types")
+first_tab_left_frame_title.pack(anchor="w")
+
+first_tab_left_frame_option_1 = tk.Checkbutton(first_tab_left_frame_subframe_2, text="Multiple choice", variable=is_multiple_choice)
+first_tab_left_frame_option_1.pack(anchor="w")
+
+first_tab_left_frame_option_2 = tk.Checkbutton(first_tab_left_frame_subframe_2, text="True / false", variable=is_true_false)
+first_tab_left_frame_option_2.pack(anchor="w")
+
+first_tab_left_frame_option_3 = tk.Checkbutton(first_tab_left_frame_subframe_2, text="Numeric", variable=is_numeric)
+first_tab_left_frame_option_3.pack(anchor="w")
+
+first_tab_left_frame_option_4 = tk.Checkbutton(first_tab_left_frame_subframe_2, text="One answer 4", variable=is_one_answer)
+first_tab_left_frame_option_4.pack(anchor="w")
+
+first_tab_center_frame_title_1 = tk.Label(first_tab_center_frame_subframe_1, text="Demo data")
+first_tab_center_frame_title_1.pack(anchor="w")
+
+first_tab_center_frame_option_1 = tk.Checkbutton(first_tab_center_frame_subframe_1, text="Include demo data", variable=is_demo_data)
+first_tab_center_frame_option_1.pack(anchor="w")
+
+first_tab_center_frame_title_2 = tk.Label(first_tab_center_frame_subframe_2, text="File path")
+first_tab_center_frame_title_2.pack(anchor="w")
+
+first_tab_center_frame_text_block = tk.Entry(first_tab_center_frame_subframe_2)
+first_tab_center_frame_text_block.pack(fill=tk.BOTH, expand=True)
+
+first_tab_center_frame_button = tk.Button(first_tab_center_frame_subframe_2, text="Browse")
+first_tab_center_frame_button.pack(fill=tk.BOTH, expand=True, padx=10)
 
 # Ejecución de la ventana principal
 root.mainloop()
