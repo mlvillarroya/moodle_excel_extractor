b9d5ec064b3b202a8bb0f0333118886c3bff155c
mlvillarroya <mlvillarroya@gmail.com>
2024-03-08 18:20:19 +0100

    added support to multiple os in opening folders

--- misc/ExplorerOpen.py
+++ misc/ExplorerOpen.py
@@ -1,14 +1,15 @@
 import subprocess
 import os
+import sys
 
 
 def open_folder(path):
-    import sys
     if 'linux' in sys.platform:
-        os.system('xdg-open "%s"' % path)
+        subprocess.Popen(['xdg-open',path])
     elif 'win32' in sys.platform:
         os.startfile(path)  # type: ignore
     elif 'darwin' in sys.platform:
-        os.system('open "%s"' % path)
+        subprocess.Popen(['open',path])
     else:
         raise RuntimeError("Unsupported operating system: {}".format(sys.platform))
+    
